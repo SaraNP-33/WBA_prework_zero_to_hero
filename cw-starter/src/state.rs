09dc4a49986409174597327bdbf2fde537e9b6a1
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2022-11-04 11:34:01 -0100

    modified state struct in state.rs and added 2 more one to account for the poll and the other the ballot

--- cw-starter/src/state.rs
+++ cw-starter/src/state.rs
@@ -1,13 +1,24 @@
+// allows structs to be serialized and deserialized to and from JSON
 use schemars::JsonSchema;
+//provide the serialization described above.
 use serde::{Deserialize, Serialize};
 
+//Cosmos address, under the hood it is simply a string
 use cosmwasm_std::Addr;
+//helper provided by storage plus. It effectively means we can store an item in storage. In this case, the STATE variable is an Item that stores a singular State struct.
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct Config {
+    pub admin: Addr
+}
+pub struct Poll{
+    pub creator: Addr,
+    pub question: String,
+    pub options: Vec<(String, u64)>
+}
+pub struct Ballot{
+    pub option: String,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const CONFIG: Item<Config> = Item::new("config");
